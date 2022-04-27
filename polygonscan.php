96e88bebb08b90dde3b74cb345a65123f03afe66
HIMANSHU GAUTAM <himang305@gmail.com>
2022-04-27 10:54:11 +0530

    Update polygonscan.php

--- polygonscan.php
+++ polygonscan.php
@@ -1,6 +1,7 @@
 
 
 $hash = '0x2f07f36176409eff4442c76520b938e288c3494de57f3de9decbd1abc5ccb73e';
+    // transaction hash to find its status 
     
 $API = 'https://api-testnet.polygonscan.com/api?module=transaction&action=gettxreceiptstatus&txhash='.$hash.'&apikey=CT3YM4K58YF4URCWY8XX47WAFYXWRUQRUQ';  
     // apikey after registering depends on plan we subscribe to
