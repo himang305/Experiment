44ec4b9e305525eb47b16293ae764210fc93558a
HIMANSHU GAUTAM <himang305@gmail.com>
2022-04-27 10:53:32 +0530

    Create polygonscan.php

---
+++ polygonscan.php
@@ -0,0 +1,12 @@
+
+
+$hash = '0x2f07f36176409eff4442c76520b938e288c3494de57f3de9decbd1abc5ccb73e';
+    
+$API = 'https://api-testnet.polygonscan.com/api?module=transaction&action=gettxreceiptstatus&txhash='.$hash.'&apikey=CT3YM4K58YF4URCWY8XX47WAFYXWRUQRUQ';  
+    // apikey after registering depends on plan we subscribe to
+
+$response = file_get_contents($API);
+$response = json_decode($response);
+ 
+print_r($response->result->status);
+
